880f5fa7e23ce97e61f4c85fd94b424247f4f374
Рифат Шайхов <rifatshaykho@gmail.com>
2025-09-30 22:11:51 +0400

    сделал что при нажатии срабатывает кнопка

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const block = document.querySelector('.blockIcons')
 
 const btn = document.querySelector('.button1');
 
+let flag = true;
+
 btn.addEventListener('click', () => {
-    block.style.backgroundColor = '#C9C0FF'
-    btn.innerHTML = '<img src="images/checkbox.svg">';
+    if (flag) {
+        block.style.backgroundColor = '#C9C0FF';
+        btn.innerHTML = '<img src="images/checkbox.svg">';
+        flag = false;        
+    } else {
+        block.style.backgroundColor = 'white';
+        btn.innerHTML = '<img src="images/box.svg">';
+        flag = true;
+    }
 });
