484eaed32f29db0787114ba2cdf40a3498bba15c
Рифат Шайхов <rifatshaykho@gmail.com>
2025-10-01 21:11:53 +0400

    Добавлени forEach в код

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-const block = document.querySelector('.blockIcons')
+const blocks = document.querySelectorAll('.blockIcons')
 
-const btn = document.querySelector('.button1');
+const button = document.querySelectorAll('.button1');
 
 let flag = true;
 
-btn.addEventListener('click', () => {
-    if (flag) {
-        block.style.backgroundColor = '#C9C0FF';
-        btn.innerHTML = '<img src="images/checkbox.svg">';
-        flag = false;        
-    } else {
-        block.style.backgroundColor = 'white';
-        btn.innerHTML = '<img src="images/box.svg">';
-        flag = true;
-    }
+//btn.addEventListener('click', () => {
+//     if (flag) {
+//         block.style.backgroundColor = '#C9C0FF';
+//         btn.innerHTML = '<img src="images/checkbox.svg">';
+//         flag = false;        
+//     } else {
+//         block.style.backgroundColor = 'white';
+//         btn.innerHTML = '<img src="images/box.svg">';
+//         flag = true;
+//     }
+//});
+
+button.forEach(btn => {
+    btn.addEventListener('click', () => {
+        // button.forEach(i => i.classList.remove('active'));
+        // this.classList.add('active');
+        if (flag) {
+            blocks.forEach(block => {
+                block.style.backgroundColor = '#C9C0FF';
+            });
+            btn.innerHTML = '<img src="images/checkbox.svg">';
+            flag = false;        
+        } else {
+            blocks.forEach(block => {
+                block.style.backgroundColor = 'white';
+            });
+            btn.innerHTML = '<img src="images/box.svg">';
+            flag = true;
+        };
+    });
 });
