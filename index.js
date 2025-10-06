8d52f625e0b735ec5540018c46c2b364e2007cae
Рифат Шайхов <rifatshaykho@gmail.com>
2025-10-06 20:43:14 +0400

    Решение вопроса по кликам и изменению цвета области контакта

--- index.js
+++ index.js
@@ -2,24 +2,16 @@ const blocks = document.querySelectorAll('.blockIcons')
 
 const button = document.querySelectorAll('.button1');
 
-let flag = true;
+let flag;
 
-//btn.addEventListener('click', () => {
-//     if (flag) {
-//         block.style.backgroundColor = '#C9C0FF';
-//         btn.innerHTML = '<img src="images/checkbox.svg">';
-//         flag = false;        
-//     } else {
-//         block.style.backgroundColor = 'white';
-//         btn.innerHTML = '<img src="images/box.svg">';
-//         flag = true;
-//     }
-//});
 
 button.forEach(btn => {
     btn.addEventListener('click', () => {
-        // button.forEach(i => i.classList.remove('active'));
-        // this.classList.add('active');
+        if (btn.innerHTML === '<img src="images/box.svg">') {
+            flag = true;
+        } else if (btn.innerHTML === '<img src="images/checkbox.svg">') {
+            flag = false;
+        }
         if (flag) {
             blocks.forEach(block => {
                 block.style.backgroundColor = '#C9C0FF';
