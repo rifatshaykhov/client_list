393bc12a996ba062259fbd34d1026135812843c1
Рифат Шайхов <rifatshaykho@gmail.com>
2025-10-07 20:46:19 +0400

    фигня

--- index.js
+++ index.js
@@ -2,28 +2,13 @@ const blocks = document.querySelectorAll('.blockIcons')
 
 const button = document.querySelectorAll('.button1');
 
-let flag;
-
 
 button.forEach(btn => {
     btn.addEventListener('click', () => {
         if (btn.innerHTML === '<img src="images/box.svg">') {
-            flag = true;
-        } else if (btn.innerHTML === '<img src="images/checkbox.svg">') {
-            flag = false;
-        }
-        if (flag) {
-            blocks.forEach(block => {
-                block.style.backgroundColor = '#C9C0FF';
-            });
             btn.innerHTML = '<img src="images/checkbox.svg">';
-            flag = false;        
         } else {
-            blocks.forEach(block => {
-                block.style.backgroundColor = 'white';
-            });
             btn.innerHTML = '<img src="images/box.svg">';
-            flag = true;
         };
-    });
+    })        
 });
