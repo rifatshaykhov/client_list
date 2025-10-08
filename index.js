ea717c11a00e8db5d1acb75bca0479761ad8f6b5
Рифат Шайхов <rifatshaykho@gmail.com>
2025-10-08 17:43:09 +0400

    клик на кнопку меняет и цвет кнопки и цвет области контакта

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const button = document.querySelectorAll('.button1');
 
 button.forEach(btn => {
     btn.addEventListener('click', () => {
+        const parentBlock = btn.parentElement;
         if (btn.innerHTML === '<img src="images/box.svg">') {
             btn.innerHTML = '<img src="images/checkbox.svg">';
+            if (parentBlock) {
+                parentBlock.style.backgroundColor = '#C9C0FF';
+            }
         } else {
             btn.innerHTML = '<img src="images/box.svg">';
+            if (parentBlock) {
+                parentBlock.style.backgroundColor = 'white';
+            };
         };
     })        
 });
