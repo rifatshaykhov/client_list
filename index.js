a5baee426267ded555e5b311f4f9139cce4f361d
Рифат Шайхов <rifatshaykho@gmail.com>
2025-10-08 18:12:06 +0400

    Добавил работу btn0

--- index.js
+++ index.js
@@ -2,20 +2,46 @@ const blocks = document.querySelectorAll('.blockIcons')
 
 const button = document.querySelectorAll('.button1');
 
+const btn0 = document.querySelector('#button');
+
 
 button.forEach(btn => {
     btn.addEventListener('click', () => {
+
         const parentBlock = btn.parentElement;
+
         if (btn.innerHTML === '<img src="images/box.svg">') {
             btn.innerHTML = '<img src="images/checkbox.svg">';
             if (parentBlock) {
                 parentBlock.style.backgroundColor = '#C9C0FF';
-            }
+            };
         } else {
             btn.innerHTML = '<img src="images/box.svg">';
             if (parentBlock) {
                 parentBlock.style.backgroundColor = 'white';
             };
         };
-    })        
+
+    });        
+});
+
+btn0.addEventListener('click', () => {
+    if (btn0.innerHTML === '<img src="images/box.svg">') {
+        btn0.innerHTML = '<img src="images/checkbox.svg">';
+
+    button.forEach(btn => {
+
+        const parentBlock = btn.parentElement;
+
+        if (btn.innerHTML === '<img src="images/box.svg">') {
+            btn.innerHTML = '<img src="images/checkbox.svg">';
+            if (parentBlock) {
+                parentBlock.style.backgroundColor = '#C9C0FF';
+            };
+        };
+    });
+
+    } else {
+        btn0.innerHTML = '<img src="images/box.svg">';    
+    };    
 });
